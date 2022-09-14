189ddaae4fdb4726f89522df5740b6d8fc965f2e
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-09-13 23:20:01 -0700

    addOrFinish recursion logic, class instantiation, class filter logic

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const gen = require('./src/template');
-// const genEngineer = require('./src/template')
+const { generateManagerHTML, generateEngineerHTML, generateInternHTML, generateHTML } = require('./src/template');
+const Engineer = require('./lib/Engineer');
+
+// import { generateManagerHTML } from './src/template.js';
+
+const members = [];
+
+var addOrFinish = {
+  type: 'list',
+  message: 'Select one of the following',
+  name: 'addOrFinish',
+  choices: [
+        'I want to add an engineer to my team', 
+        'I want to add an intern to my team', 
+        'Finish building my team', 
+        ],
+};
 
 var managerQuestions = [
   {
@@ -24,17 +39,7 @@ var managerQuestions = [
     name: 'managerOfficeNumber',
     message: 'What is your team manager’s office number?',
   },
-  {
-    type: 'list',
-    message: 'Team Manager profile created. Select one of the following:',
-    name: 'managerComplete',
-    choices: [
-    'I want to add an engineer to my team', 
-    'I want to add an intern to my team', 
-    'Finish building my team', 
-    ],
-},
-  ]
+  ];
 
   var engineerQuestions = [
     {
@@ -57,47 +62,99 @@ var managerQuestions = [
       name: 'engineerGithub',
       message: 'What is your engineer’s Github username?',
     },
-    {
-      type: 'list',
-      message: 'Engineer profile created.\nSelect one of the following:',
-      name: 'engineerComplete',
-      choices: [
-      'I want to add an intern to my team', 
-      'Finish building my team', 
-      ],
-  },
     ]
 
+    var internQuestions = [
+      {
+        type: 'input',
+        name: 'internName',
+        message: 'What is your intern’s name?',
+      },
+      {
+        type: 'input',
+        name: 'internId',
+        message: 'What is your intern’s employee ID?',
+      },
+      {
+        type: 'input',
+        name: 'internEmail',
+        message: 'What is your intern’s email?',
+      },
+      {
+        type: 'input',
+        name: 'internSchool',
+        message: 'Which school does your intern attend?',
+      },
+      ]
   // user builds team profile
  function ask() {
 
-  // inquirer prompts for team's manager information
-  inquirer.prompt(managerQuestions)
-  .then((managerAnswers) => {
+  // User asked for team manager's information
+  inquirer.prompt(managerQuestions).then((managerAnswers) => {
 
-    // user wants to add engineer after adding manager
-    if (managerAnswers.managerComplete == 'I want to add an engineer to my team') {
-      console.log('User wants to add an engineer to their team')
-
-      // ask engineer prompts
-      inquirer.prompt(engineerQuestions)
-      .then((engineerAnswers) =>{
-        console.log(engineerAnswers)
-        const engineerContent = gen(engineerAnswers);
-        fs.appendFile('./dist/index.html', engineerContent, (err) =>
-        err ? console.log(err) : console.log('Successfully added team manager profile!'));
-      })
-
-      // if user chooses to finish building team, then generate HTML with manager
-    } else {
-
-      // manager answers as input into generateMangerHTML function
-      const managerContent = gen(managerAnswers);
-      fs.writeFile('./dist/index.html', managerContent, (err) =>
-        err ? console.log(err) : console.log('Successfully added team manager profile!'));
-    };
-
+    // manager answers as input into generateManagerHTML function
+    // const managerContent = generateManagerHTML(managerAnswers);
+    members.push(managerAnswers);
+    
+    askContinue(managerAnswers);
+    // generateHTML(members);
   });
- } 
+};
+
+function askContinue(managerAnswers) {
+  // console.log(members);
+
+  // Ask whether user to add a member or complete team profile
+  inquirer.prompt(addOrFinish).then((answer) => {
+
+    // If user adds an engineer
+    if (answer.addOrFinish == 'I want to add an engineer to my team') {
+
+        // Ask engineer prompts
+        inquirer.prompt(engineerQuestions).then((engineerAnswers) =>{
+
+          // Format engineer answers
+          // const engineerContent = generateEngineerHTML(engineerAnswers);
+
+          console.log(engineerAnswers);
+          const engineer = new Engineer(engineerAnswers.engineerName, engineerAnswers.engineerId, engineerAnswers.engineerEmail, engineerAnswers.engineerGithub)
+          members.push(engineer);
+          
+          askContinue(managerAnswers);
+        });
+      
+        // If user adds an intern
+      } else if (answer.addOrFinish == 'I want to add an intern to my team') {
+
+        // Ask intern prompts
+        inquirer.prompt(internQuestions).then((internAnswers) =>{
+          console.log(internAnswers);
+          // const internContent = generateInternHTML(internAnswers);
+          members.push(internAnswers);
+          askContinue(managerAnswers);
+        });
+
+        // User finishes building team and HTML is generated
+       } else {
+          console.log('Members of your team')
+          console.log(members);
+          generateHTML(members);
+          // Manager answers are formatted then written to file
+          // const managerContent = generateManagerHTML(managerAnswers);
+          // fs.writeFile('./dist/index.html', managerContent, (err) =>
+          // err ? console.log(err) : console.log('Successfully created your team profile!'));
+        };
+  });
+};
+
+
+        // manager answers as input into generateMangerHTML function
+        // const managerContent = gen(managerAnswers);
+
+  
+      // append engineer logic
+      // fs.appendFile('./dist/index.html', engineerContent, (err) =>
+      //     err ? console.log(err) : console.log('Successfully added engineers profiles!'));
+
 
  ask();
--- src/template.js
+++ src/template.js
@@ -1,12 +1,14 @@
 // import Manager class from lib directory
 const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
 
 // format manager answers then write to index.html
 function generateManagerHTML (managerAnswers) {
-
+ 
   // instantiate a Manager using user's manager answers
   const manager = new Manager(managerAnswers.managerName, managerAnswers.managerId, managerAnswers.managerEmail, managerAnswers.managerOfficeNumber);
-
+  console.log(manager);
   const managerFormatted = 
     `<!DOCTYPE html>
   <html lang="en">
@@ -33,4 +35,54 @@ function generateManagerHTML (managerAnswers) {
 return managerFormatted;
 };
 
-module.exports = generateManagerHTML;
+function generateEngineerHTML (engineerAnswers) {
+
+  // const engineer = new Engineer(engineerAnswers.engineerName, engineerAnswers.engineerId, engineerAnswers.engineerEmail, engineerAnswers.engineerGithub)
+  const engineerFormatted = 
+    `<div class="container">
+    <h1 class="display-4">${engineer.name}</h1>
+    <h2 class="">💻${engineer.getRole()}.</h2>
+    <ul class="list-group">
+      <li class="list-group-item">ID: ${engineer.id}</li>
+      <li class="list-group-item">Contact: ${engineer.email}</li>
+      <li class="list-group-item">Office Number: ${engineer.github}</li>
+    </ul>
+  </div>`
+  return engineerFormatted;
+}
+  
+function generateInternHTML (internAnswers) {
+  const intern = new Intern(internAnswers.internName, internAnswers.internId, internAnswers.internEmail, internAnswers.internGithub)
+  const internFormatted = 
+    `<div class="container">
+    <h1 class="display-4">${intern.name}</h1>
+    <h2 class="">📛 ${intern.getRole()}.</h2>
+    <ul class="list-group">
+      <li class="list-group-item">ID: ${intern.id}</li>
+      <li class="list-group-item">Contact: ${intern.email}</li>
+      <li class="list-group-item">Office Number: ${intern.github}</li>
+    </ul>
+  </div>`;
+  return internFormatted;
+}
+
+function generateHTML (members) {
+  const engineers = [];
+  for (i=0; i < members.length; i++) {
+    console.log(members[i].constructor.name);
+    if (members[i] instanceof Engineer) {
+      engineers.push(members[i]);
+    };
+  };
+  console.log('Your Engineers:');
+  console.log(engineers);
+
+  return engineers;
+};
+
+module.exports = {
+  generateManagerHTML,
+  generateEngineerHTML,
+  generateInternHTML,
+  generateHTML,
+};
